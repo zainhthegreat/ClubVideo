3767d0b4d27b603c1189349f56a57d65ff9371fc
zainhthegreat <zainh775@gmail.com>
2022-02-18 07:26:43 -0800

    ,

---
+++ lib/models/File.dart
@@ -0,0 +1,298 @@
+/*
+* Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+// NOTE: This file is generated and may not follow lint rules defined in your app
+// Generated files can be excluded from analysis in analysis_options.yaml
+// For more info, see: https://dart.dev/guides/language/analysis-options#excluding-code-from-analysis
+
+// ignore_for_file: public_member_api_docs, file_names, unnecessary_new, prefer_if_null_operators, prefer_const_constructors, slash_for_doc_comments, annotate_overrides, non_constant_identifier_names, unnecessary_string_interpolations, prefer_adjacent_string_concatenation, unnecessary_const, dead_code
+
+import 'ModelProvider.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/foundation.dart';
+
+
+/** This is an auto generated class representing the File type in your schema. */
+@immutable
+class File extends Model {
+  static const classType = const _FileModelType();
+  final String id;
+  final String? _Name;
+  final FileType? _Type;
+  final String? _category;
+  final String? _description;
+  final String? _ownerID;
+  final int? _Grade;
+  final TemporalDateTime? _createdAt;
+  final TemporalDateTime? _updatedAt;
+
+  @override
+  getInstanceType() => classType;
+  
+  @override
+  String getId() {
+    return id;
+  }
+  
+  String get Name {
+    try {
+      return _Name!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  FileType get Type {
+    try {
+      return _Type!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  String get category {
+    try {
+      return _category!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  String? get description {
+    return _description;
+  }
+  
+  String get ownerID {
+    try {
+      return _ownerID!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  int get Grade {
+    try {
+      return _Grade!;
+    } catch(e) {
+      throw new AmplifyCodeGenModelException(
+          AmplifyExceptionMessages.codeGenRequiredFieldForceCastExceptionMessage,
+          recoverySuggestion:
+            AmplifyExceptionMessages.codeGenRequiredFieldForceCastRecoverySuggestion,
+          underlyingException: e.toString()
+          );
+    }
+  }
+  
+  TemporalDateTime? get createdAt {
+    return _createdAt;
+  }
+  
+  TemporalDateTime? get updatedAt {
+    return _updatedAt;
+  }
+  
+  const File._internal({required this.id, required Name, required Type, required category, description, required ownerID, required Grade, createdAt, updatedAt}): _Name = Name, _Type = Type, _category = category, _description = description, _ownerID = ownerID, _Grade = Grade, _createdAt = createdAt, _updatedAt = updatedAt;
+  
+  factory File({String? id, required String Name, required FileType Type, required String category, String? description, required String ownerID, required int Grade}) {
+    return File._internal(
+      id: id == null ? UUID.getUUID() : id,
+      Name: Name,
+      Type: Type,
+      category: category,
+      description: description,
+      ownerID: ownerID,
+      Grade: Grade);
+  }
+  
+  bool equals(Object other) {
+    return this == other;
+  }
+  
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    return other is File &&
+      id == other.id &&
+      _Name == other._Name &&
+      _Type == other._Type &&
+      _category == other._category &&
+      _description == other._description &&
+      _ownerID == other._ownerID &&
+      _Grade == other._Grade;
+  }
+  
+  @override
+  int get hashCode => toString().hashCode;
+  
+  @override
+  String toString() {
+    var buffer = new StringBuffer();
+    
+    buffer.write("File {");
+    buffer.write("id=" + "$id" + ", ");
+    buffer.write("Name=" + "$_Name" + ", ");
+    buffer.write("Type=" + (_Type != null ? enumToString(_Type)! : "null") + ", ");
+    buffer.write("category=" + "$_category" + ", ");
+    buffer.write("description=" + "$_description" + ", ");
+    buffer.write("ownerID=" + "$_ownerID" + ", ");
+    buffer.write("Grade=" + (_Grade != null ? _Grade!.toString() : "null") + ", ");
+    buffer.write("createdAt=" + (_createdAt != null ? _createdAt!.format() : "null") + ", ");
+    buffer.write("updatedAt=" + (_updatedAt != null ? _updatedAt!.format() : "null"));
+    buffer.write("}");
+    
+    return buffer.toString();
+  }
+  
+  File copyWith({String? id, String? Name, FileType? Type, String? category, String? description, String? ownerID, int? Grade}) {
+    return File._internal(
+      id: id ?? this.id,
+      Name: Name ?? this.Name,
+      Type: Type ?? this.Type,
+      category: category ?? this.category,
+      description: description ?? this.description,
+      ownerID: ownerID ?? this.ownerID,
+      Grade: Grade ?? this.Grade);
+  }
+  
+  File.fromJson(Map<String, dynamic> json)  
+    : id = json['id'],
+      _Name = json['Name'],
+      _Type = enumFromString<FileType>(json['Type'], FileType.values),
+      _category = json['category'],
+      _description = json['description'],
+      _ownerID = json['ownerID'],
+      _Grade = (json['Grade'] as num?)?.toInt(),
+      _createdAt = json['createdAt'] != null ? TemporalDateTime.fromString(json['createdAt']) : null,
+      _updatedAt = json['updatedAt'] != null ? TemporalDateTime.fromString(json['updatedAt']) : null;
+  
+  Map<String, dynamic> toJson() => {
+    'id': id, 'Name': _Name, 'Type': enumToString(_Type), 'category': _category, 'description': _description, 'ownerID': _ownerID, 'Grade': _Grade, 'createdAt': _createdAt?.format(), 'updatedAt': _updatedAt?.format()
+  };
+
+  static final QueryField ID = QueryField(fieldName: "file.id");
+  static final QueryField NAME = QueryField(fieldName: "Name");
+  static final QueryField TYPE = QueryField(fieldName: "Type");
+  static final QueryField CATEGORY = QueryField(fieldName: "category");
+  static final QueryField DESCRIPTION = QueryField(fieldName: "description");
+  static final QueryField OWNERID = QueryField(fieldName: "ownerID");
+  static final QueryField GRADE = QueryField(fieldName: "Grade");
+  static var schema = Model.defineSchema(define: (ModelSchemaDefinition modelSchemaDefinition) {
+    modelSchemaDefinition.name = "File";
+    modelSchemaDefinition.pluralName = "Files";
+    
+    modelSchemaDefinition.authRules = [
+      AuthRule(
+        authStrategy: AuthStrategy.PUBLIC,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ]),
+      AuthRule(
+        authStrategy: AuthStrategy.OWNER,
+        ownerField: "owner",
+        identityClaim: "cognito:username",
+        provider: AuthRuleProvider.USERPOOLS,
+        operations: [
+          ModelOperation.CREATE,
+          ModelOperation.UPDATE,
+          ModelOperation.DELETE,
+          ModelOperation.READ
+        ])
+    ];
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.id());
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: File.NAME,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: File.TYPE,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.enumeration)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: File.CATEGORY,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: File.DESCRIPTION,
+      isRequired: false,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: File.OWNERID,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.string)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.field(
+      key: File.GRADE,
+      isRequired: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.int)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+      fieldName: 'createdAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+    
+    modelSchemaDefinition.addField(ModelFieldDefinition.nonQueryField(
+      fieldName: 'updatedAt',
+      isRequired: false,
+      isReadOnly: true,
+      ofType: ModelFieldType(ModelFieldTypeEnum.dateTime)
+    ));
+  });
+}
+
+class _FileModelType extends ModelType<File> {
+  const _FileModelType();
+  
+  @override
+  File fromJson(Map<String, dynamic> jsonData) {
+    return File.fromJson(jsonData);
+  }
+}
